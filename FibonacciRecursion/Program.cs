6885c7cf52bcaa42ae63a1b9aa362efca6266611
Jakub Berthoty <jakub.berthoty@gmail.com>
2019-04-28 22:27:16 +0200

    Fibonacci - recursion.

--- FibonacciRecursion/Program.cs
+++ FibonacciRecursion/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FibonacciRecursion
 {
@@ -10,6 +6,28 @@ namespace FibonacciRecursion
     {
         static void Main(string[] args)
         {
+            Console.Write("Insert Fibonacci sequence index: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(Fibonacci(n));
+
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Calculates n-th Fibonacci member.
+        /// </summary>
+        /// <param name="n">Index.</param>
+        /// <returns>N-th Fibonacci member.</returns>
+        private static long Fibonacci(int n)
+        {
+            if (n <= 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return Fibonacci(n - 1) + Fibonacci(n - 2);
+            }
         }
     }
 }
