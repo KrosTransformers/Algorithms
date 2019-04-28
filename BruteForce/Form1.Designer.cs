2caa637eb2e1b68047a41cbbefee6cd30caf4ef6
Jakub Berthoty <jakub.berthoty@gmail.com>
2019-04-28 23:43:09 +0200

    Brute force - password cracker.

---
+++ BruteForce/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace BruteForce
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCrack = new System.Windows.Forms.Button();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.chkUpper = new System.Windows.Forms.CheckBox();
+            this.chkLower = new System.Windows.Forms.CheckBox();
+            this.chkDigits = new System.Windows.Forms.CheckBox();
+            this.txtCrackedPassword = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtMinLength = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtMaxLength = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Password:";
+            // 
+            // btnCrack
+            // 
+            this.btnCrack.Location = new System.Drawing.Point(15, 87);
+            this.btnCrack.Name = "btnCrack";
+            this.btnCrack.Size = new System.Drawing.Size(250, 23);
+            this.btnCrack.TabIndex = 10;
+            this.btnCrack.Text = "CRACK!";
+            this.btnCrack.UseVisualStyleBackColor = true;
+            this.btnCrack.Click += new System.EventHandler(this.btnCrack_Click);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(96, 12);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(169, 20);
+            this.txtPassword.TabIndex = 1;
+            // 
+            // chkUpper
+            // 
+            this.chkUpper.AutoSize = true;
+            this.chkUpper.Checked = true;
+            this.chkUpper.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkUpper.Location = new System.Drawing.Point(15, 38);
+            this.chkUpper.Name = "chkUpper";
+            this.chkUpper.Size = new System.Drawing.Size(49, 17);
+            this.chkUpper.TabIndex = 2;
+            this.chkUpper.Text = "A - Z";
+            this.chkUpper.UseVisualStyleBackColor = true;
+            // 
+            // chkLower
+            // 
+            this.chkLower.AutoSize = true;
+            this.chkLower.Checked = true;
+            this.chkLower.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkLower.Location = new System.Drawing.Point(70, 38);
+            this.chkLower.Name = "chkLower";
+            this.chkLower.Size = new System.Drawing.Size(46, 17);
+            this.chkLower.TabIndex = 3;
+            this.chkLower.Text = "a - z";
+            this.chkLower.UseVisualStyleBackColor = true;
+            // 
+            // chkDigits
+            // 
+            this.chkDigits.AutoSize = true;
+            this.chkDigits.Checked = true;
+            this.chkDigits.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkDigits.Location = new System.Drawing.Point(122, 38);
+            this.chkDigits.Name = "chkDigits";
+            this.chkDigits.Size = new System.Drawing.Size(47, 17);
+            this.chkDigits.TabIndex = 4;
+            this.chkDigits.Text = "0 - 9";
+            this.chkDigits.UseVisualStyleBackColor = true;
+            // 
+            // txtCrackedPassword
+            // 
+            this.txtCrackedPassword.BackColor = System.Drawing.SystemColors.Window;
+            this.txtCrackedPassword.Location = new System.Drawing.Point(96, 116);
+            this.txtCrackedPassword.Name = "txtCrackedPassword";
+            this.txtCrackedPassword.ReadOnly = true;
+            this.txtCrackedPassword.Size = new System.Drawing.Size(169, 20);
+            this.txtCrackedPassword.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Password:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 145);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Time ellapsed:";
+            // 
+            // lblTime
+            // 
+            this.lblTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblTime.Location = new System.Drawing.Point(93, 143);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(75, 20);
+            this.lblTime.TabIndex = 14;
+            this.lblTime.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(174, 145);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "ms";
+            // 
+            // txtMinLength
+            // 
+            this.txtMinLength.Location = new System.Drawing.Point(80, 61);
+            this.txtMinLength.MaxLength = 2;
+            this.txtMinLength.Name = "txtMinLength";
+            this.txtMinLength.Size = new System.Drawing.Size(20, 20);
+            this.txtMinLength.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 64);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Min length:";
+            // 
+            // txtMaxLength
+            // 
+            this.txtMaxLength.Location = new System.Drawing.Point(176, 61);
+            this.txtMaxLength.MaxLength = 2;
+            this.txtMaxLength.Name = "txtMaxLength";
+            this.txtMaxLength.Size = new System.Drawing.Size(20, 20);
+            this.txtMaxLength.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(108, 64);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(62, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Max length:";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 176);
+            this.Controls.Add(this.txtMaxLength);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtMinLength);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtCrackedPassword);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.chkDigits);
+            this.Controls.Add(this.chkLower);
+            this.Controls.Add(this.chkUpper);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.btnCrack);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.ShowIcon = false;
+            this.Text = "Brute force";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCrack;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.CheckBox chkUpper;
+        private System.Windows.Forms.CheckBox chkLower;
+        private System.Windows.Forms.CheckBox chkDigits;
+        private System.Windows.Forms.TextBox txtCrackedPassword;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtMinLength;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtMaxLength;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ BruteForce/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace BruteForce
+{
+    public partial class frmMain : Form
+    {
+
+        #region Constants
+
+        private const string LOWER = "abcdefghijklmnopqrstuvwxyz";
+        private const string UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        private const string DIGITS = "0123456789";
+
+        #endregion
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCrack_Click(object sender, EventArgs e)
+        {
+            int minLength = (string.IsNullOrEmpty(txtMinLength.Text) ? 1 : int.Parse(txtMinLength.Text));
+            int maxLength = (string.IsNullOrEmpty(txtMaxLength.Text) ? 100 : int.Parse(txtMaxLength.Text));
+            string alphabet = $"{(chkLower.Checked ? LOWER : "")}{(chkUpper.Checked ? UPPER : "")}{(chkDigits.Checked ? DIGITS : "")}";
+
+            if (!string.IsNullOrEmpty(alphabet))
+            {
+                txtCrackedPassword.Text = "### NOT FOUND ###";
+                lblTime.Text = " - ";
+                Enabled = false;
+                Cursor = Cursors.WaitCursor;
+
+                DateTime startTime = DateTime.Now;
+                for (int l = minLength; l <= maxLength; l++)
+                {
+                    char[] word = new char[l];
+                    for (int i = 0; i < l; i++)
+                    {
+                        word[i] = alphabet[0];
+                    }
+
+                    if (CheckPassword(word, 0, alphabet, txtPassword.Text))
+                    {
+                        txtCrackedPassword.Text = string.Join("", word);                        
+                        break;
+                    }
+                }
+
+                Cursor = Cursors.Default;
+                Enabled = true;
+                lblTime.Text = (DateTime.Now - startTime).TotalMilliseconds.ToString();
+            }
+        }
+
+        /// <summary>
+        /// Tries to recursively find a password.
+        /// </summary>
+        /// <param name="word">Password suggestion.</param>
+        /// <param name="index">Current letter.</param>
+        /// <param name="alphabet">Allowed characters.</param>
+        /// <param name="password">Password.</param>
+        /// <returns>True, if provided word is password; otherwise false.</returns>
+        private bool CheckPassword(char[] word, int index, string alphabet, string password)
+        {
+            if (string.Join("", word) == password)
+            {
+                return true;
+            }
+            else if (index == word.Length)
+            {
+                return false;
+            }
+            else
+            {
+                foreach (char letter in alphabet)
+                {
+                    word[index] = letter;
+                    if (CheckPassword(word, index + 1, alphabet, password))
+                    {
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+        }
+    }
+}
