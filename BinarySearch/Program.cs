145bef8903785512bd13a0ae2cf4ecb438f09a53
Jakub Berthoty <berthoty@kros.sk>
2019-04-29 11:45:19 +0200

    Divide and conquer - Binary Search.

---
+++ BinarySearch/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+
+namespace BinarySearch
+{
+    class Program
+    {
+        #region Constants
+
+        /// <summary>
+        /// Min number for generating random data.
+        /// </summary>
+        private const int MIN_NUMBER = -1000;
+        /// <summary>
+        /// Max number for generating random data.
+        /// </summary>
+        private const int MAX_NUMBER =  1000;
+
+        #endregion
+
+        static void Main(string[] args)
+        {
+            Console.Write("Number of elements: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write($"Element to search for ({MIN_NUMBER} - {MAX_NUMBER}): ");
+            int x = int.Parse(Console.ReadLine());            
+
+            int[] data = GenerateRandomArray(n);
+            Console.WriteLine("Data: ");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"{data[i],5} ");
+                if ((i + 1) % 10 == 0)
+                {
+                    Console.WriteLine();
+                }
+            }
+            Console.WriteLine();
+
+            int index = BinarySearch(data, 0, data.Length - 1, x);
+            if (index > 0)
+            {
+                Console.WriteLine($"Item was found at index {index}.");
+            }
+            else
+            {
+                Console.WriteLine("Item was not found.");
+            }
+
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Tries to search for item in specified range of data array.
+        /// </summary>
+        /// <param name="data">Data array.</param>
+        /// <param name="startIndex">Start of searched interval.</param>
+        /// <param name="endIndex">End of searched interval.</param>
+        /// <param name="x">Searched for item.</param>
+        /// <returns>Item index in data array; -1 if array does not contain item.</returns>
+        private static int BinarySearch(int[] data, int startIndex, int endIndex, int x)
+        {
+            int middleIndex = FindMiddleIndex(startIndex, endIndex);
+            int middleItem = data[middleIndex];
+
+            if (x == middleItem)
+            {
+                return middleIndex;
+            }
+            else if (x > middleItem)
+            {
+                if (middleIndex == endIndex)
+                {
+                    return -1;
+                }
+                else
+                {
+                    return BinarySearch(data, middleIndex + 1, endIndex, x);
+                }
+            }
+            else
+            {
+                if (middleIndex == startIndex)
+                {
+                    return -1;
+                }
+                else
+                {
+                    return BinarySearch(data, startIndex, middleIndex - 1, x);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Generates sorted random array with specific length.
+        /// </summary>
+        /// <param name="length">Length of array.</param>
+        /// <returns>Sorted array with random numbers.</returns>
+        private static int[] GenerateRandomArray(int length)
+        {
+            int[] ret = new int[length];
+
+            Random r = new Random(Environment.TickCount);
+            for (int i = 0; i < length; i++)
+            {
+                ret[i] = r.Next(MIN_NUMBER, MAX_NUMBER + 1);
+            }
+            QuickSort.Sort(ret);
+
+            return ret;
+        }
+
+        /// <summary>
+        /// Find most middle index from interval.
+        /// </summary>
+        /// <param name="startIndex">Interval start.</param>
+        /// <param name="endIndex">Interval end.</param>
+        /// <returns></returns>
+        private static int FindMiddleIndex(int startIndex, int endIndex)
+        {
+            return startIndex + (endIndex - startIndex) / 2;
+        }
+    }
+}
---
+++ BinarySearch/QuickSort.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace BinarySearch
+{
+
+    /// <summary>
+    /// Quick Sort algorithm implementation.
+    /// </summary>
+    class QuickSort
+    {
+
+        /// <summary>
+        /// Sorts data in ascending order.
+        /// </summary>
+        /// <param name="data">Data to be sorted.</param>
+        /// <returns>Number of operations required to sort the data.</returns>
+        public static long Sort(int[] data)
+        {
+            return Partition(data, 0, data.Length - 1);
+        }
+
+        /// <summary>
+        /// Partitions data for QuickSort.
+        /// </summary>
+        /// <param name="data">Data.</param>
+        /// <param name="start">Start index of data to be partitioned.</param>
+        /// <param name="end">End index of data to be partitioned.</param>
+        /// <returns>Number of operations required.</returns>
+        public static long Partition(int[] data, int start, int end)
+        {
+            long operations = 0;
+
+            if (start != end)
+            {
+                Random random = new Random(Environment.TickCount);
+                int pivotIndex = random.Next(start, end + 1);
+                int pivot = data[pivotIndex];
+
+                Swap(data, pivotIndex, start);
+                operations++;
+
+                int firstOpened = start + 1;
+                int lastClosed = start;
+                for (int i = start + 1; i <= end; i++)
+                {
+                    if (data[i] < pivot)
+                    {
+                        Swap(data, i, firstOpened);
+                        operations++;
+
+                        lastClosed = firstOpened;
+                        firstOpened++;
+                    }
+                    operations++;
+                }
+
+                Swap(data, start, lastClosed);
+                operations++;
+
+                if (lastClosed != start)
+                {
+                    operations += Partition(data, start, lastClosed - 1);
+                }
+                if (lastClosed != end)
+                {
+                    operations += Partition(data, lastClosed + 1, end);
+                }
+            }
+
+            return operations;
+        }
+
+        /// <summary>
+        /// Swaps 2 elements in a list.
+        /// </summary>
+        /// <param name="data">List.</param>
+        /// <param name="a">Index of first element.</param>
+        /// <param name="b">Index of second element.</param>
+        private static void Swap(int[] data, int a, int b)
+        {
+            int temp = data[a];
+            data[a] = data[b];
+            data[b] = temp;
+        }
+
+    }
+
+}
